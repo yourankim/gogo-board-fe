3cdf552da57e8b828e418739b6d31e6fe10e7868
yourankim <youran.dev@gmail.com>
2022-03-30 22:45:57 +0900

    token refresh 처리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Posts from './posts/posts';
-import PostDetail from './posts/post.detail';
-import Register from './register/register';
-import Login from './login/login';
-import { UserContext } from './user.context';
-import PrivateRouter from './privateRouter';
+import Posts from './pages/posts';
+import PostDetail from './pages/post.detail';
+import Register from './pages/register';
+import Login from './pages/login';
+import { UserContext } from './context/user.context';
+import PrivateRouter from './components/privateRouter';
+import { axiosInstance } from './api/axiosInstance';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
   const loginState = {
     isLoggedIn: false,
     setLoginState: (isLoggedIn: boolean) => setIsLoggedIn(isLoggedIn),
   };
-  console.log(isLoggedIn);
+  console.log('isLoggedIn in App', isLoggedIn);
+
+  const refresh = async () => {
+    try {
+      console.log('refresh');
+      const response = await axiosInstance.post('/users/auth/refresh/');
+      const accessToken = response.data;
+      axiosInstance.defaults.headers.common[
+        'Authorization'
+      ] = `Bearer ${accessToken}`;
+      setIsLoggedIn(true);
+    } catch (e: any) {
+      console.log(e.Message);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  if (loading) return <div>loading...</div>;
 
   return (
     <UserContext.Provider value={loginState}>
