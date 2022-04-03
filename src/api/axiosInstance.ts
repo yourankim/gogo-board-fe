9ba017beded12c25bd14f7ab7f712d78956d812c
yourankim <youran.dev@gmail.com>
2022-04-03 14:59:19 +0900

    공통: axios interceptor에 401 에러 처리 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -17,8 +17,15 @@ const handleSuccess = (response: any) => {
 };
 
 const handleError = (error: any) => {
-  if (error.response.status >= 500 && error.response.status < 600) {
+  const status = error.response.status;
+  const url = error.config.url;
+  if (status >= 500 && status < 600) {
     alert('일시적인 오류입니다. 잠시 후 다시 시도해주세요.');
+  } else if (status === 401 && !url.includes('/users/auth')) {
+    window.location.href = '/login';
+  } else if (status === 401 && url.includes('refresh')) {
+    // TODO: 여기서 그냥 통과시키면 isLoggedIn 상태를 바꾸지 못하는 것 아닌가? 인증 상태 처리하는 부분을 다 모아서 정리를 다시 해보자.
+    return Promise.resolve(error.response);
   }
   console.error(error);
   return Promise.reject(error);
