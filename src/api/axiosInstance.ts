777f9efa55410ab400418106d038352022a3efad
yourankim <youran.dev@gmail.com>
2022-04-24 13:57:43 +0900

    fix: 인증 관련 에러 처리 수정 #2
    인증 관련 에러는 모두 status code 401로 응답하도록 처리함
    refresh 요청에서 401 에러 발생했을 때도 로그인 페이지로 이동하도록 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -41,14 +41,8 @@ const handleError = (error: any) => {
   //refresh는 사용자가 인지하지 못하므로 오류메시지 불필요
   if (status >= 500 && status < 600 && !url.includes('refresh')) {
     alert('일시적인 오류입니다. 잠시 후 다시 시도해주세요.');
-  } else if (
-    (status === 400 || status === 401) &&
-    (!url.includes('/users/auth') || url.includes('refresh'))
-  ) {
+  } else if (status === 401 && url !== '/users/auth') {
     window.location.href = '/login';
-    // TODO: 400 에러는 쿠키에 리프레쉬 토큰 자체가 없을 때 발생. 서버에서 401로 리턴해줄 수 있지 않을까?
-    // TODO: refresh일때 그냥 통과시키는 처리 빼버렸는데 아예 빼는건 문제가 있으려나? App에서 호출하지 않고 setTimeout으로 호출했을 때는
-    // 어떤 후처리가 필요하지? 정상일 땐 처리할 게 없지만 오류가 났다면 로그인 페이지로 이동하는 게 맞는 것 같은데..
   }
   console.error(error);
   return Promise.reject(error);
