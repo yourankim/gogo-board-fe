63521a895a9ef72753df04f6eadb68564865dcea
yourankim <youran.dev@gmail.com>
2022-05-01 22:13:05 +0900

    refactor: 게시글 저장  useForm 사용해서 처리하도록 수정 #3
    id를 hidden field에 세팅해줘야 formData에 들어간다.

--- src/pages/post.editor.tsx
+++ src/pages/post.editor.tsx
@@ -4,23 +4,26 @@ import { axiosInstance } from '../api/axiosInstance';
 import Post from '../interface/post';
 import { PostForm, Input, TextArea } from '../components/style/post';
 import { Button } from '../components/style/common';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 function PostEditor() {
   const postId = useParams<{ postId: string }>().postId;
-  const [post, setPost] = React.useState<Post>({} as Post);
-  const [title, setTitle] = React.useState<string>('');
-  const [content, setContent] = React.useState<string>('');
   const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Post>();
+
   const navigate = useNavigate();
-  //TODO: 중복 fetch 추출, useForm 적용하기
+  //TODO: 중복 fetch 추출
   const getPost = useCallback(async () => {
-    console.log('postEditor getPost');
     const response = await axiosInstance.get(`/posts/${postId}`);
     const post: Post = response.data.post;
-    setPost(post);
-    setTitle(post.title);
-    setContent(post.content);
+    setValue('title', post.title);
+    setValue('content', post.content);
     setIsLoading(false);
   }, [postId]);
 
@@ -32,19 +35,15 @@ function PostEditor() {
     }
   }, [postId, getPost]);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!title || !content) {
-      alert('제목과 내용을 입력해주세요.');
+  const onSubmit: SubmitHandler<Post> = async (formData) => {
+    if (!window.confirm('글을 저장할까요?')) {
       return;
     }
-    const newPost = { ...post, title, content };
 
     if (postId) {
-      await axiosInstance.patch(`/posts`, newPost);
+      await axiosInstance.patch(`/posts`, formData);
     } else {
-      await axiosInstance.post('/posts', newPost);
+      await axiosInstance.post('/posts', formData);
     }
     alert('글을 저장했습니다.');
     navigate('/');
@@ -54,20 +53,19 @@ function PostEditor() {
     return <div>loading...</div>;
   }
   return (
-    <PostForm onSubmit={handleSubmit}>
+    <PostForm onSubmit={handleSubmit(onSubmit)}>
       <h2>글쓰기</h2>
+      {postId && (
+        <Input type='hidden' {...register('id', { value: parseInt(postId) })} />
+      )}
       <Input
         type='text'
-        name='title'
-        value={title}
         placeholder='제목을 입력하세요.'
-        onChange={(e) => setTitle(e.target.value)}
+        {...register('title', { required: true })}
       />
       <TextArea
-        name='content'
         placeholder='내용을 입력하세요.'
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
+        {...register('content', { required: true })}
       />
       <Button>저장</Button>
     </PostForm>
